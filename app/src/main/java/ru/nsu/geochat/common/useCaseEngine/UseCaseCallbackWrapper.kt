261658f57b7ac1770a45fc49975491929a82d56b
Vlad <vlad-498@yandex.ru>
2019-12-15 13:45:24 +0700

    GC-12 Create UseCaseCallbackWrapper

---
+++ app/src/main/java/ru/nsu/geochat/common/useCaseEngine/UseCaseCallbackWrapper.kt
@@ -0,0 +1,17 @@
+package ru.nsu.geochat.common.useCaseEngine
+
+class UseCaseCallbackWrapper<T: UseCase.ResponseValue> (
+    private val callback: IUseCaseCallback<T>,
+    private val useCaseHandler: UseCaseHandler
+    ): IUseCaseCallback<T>{
+
+    override fun onSuccess(response: T) {
+        useCaseHandler.notifyResponse(response, callback)
+    }
+
+    override fun onError() {
+        useCaseHandler.notifyError(callback)
+    }
+
+
+}
