174dcb7d9fa5ea1253560b20c3fd2474a5ff9a51
Vlad <vlad-498@yandex.ru>
2019-12-16 06:00:30 +0700

    CG-29 Create login activity

---
+++ app/src/main/java/ru/nsu/geochat/modules/activity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package ru.nsu.geochat.modules.activity
+
+import android.os.Bundle
+import ru.nsu.geochat.modules.base.BaseActivity
+
+class LoginActivity: BaseActivity(LoginFragment()) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val view = fragment as ILoginView
+        val presenter = LoginPresenter(view)
+        view.setPresenter(presenter)
+    }
+}
