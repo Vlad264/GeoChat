fa736d52ddeb027a2e63efc12b2911fbe339049d
Vlad <vlad-498@yandex.ru>
2019-12-15 13:44:10 +0700

    GC-13 Create UseCaseHandler

--- app/src/main/java/ru/nsu/geochat/common/useCaseEngine/UseCaseHandler.kt
+++ app/src/main/java/ru/nsu/geochat/common/useCaseEngine/UseCaseHandler.kt
@@ -1,4 +1,35 @@
 package ru.nsu.geochat.common.useCaseEngine
 
-class UseCaseHandler {
+class UseCaseHandler private constructor() {
+
+    private val useCaseScheduler : IUseCaseScheduler
+
+    init {
+        useCaseScheduler = UseCaseThreadPoolScheduler()
+    }
+
+    private object Holder {
+        val INSTANCE = UseCaseHandler()
+    }
+
+    companion object {
+        val instance by lazy { Holder.INSTANCE }
+    }
+
+    fun <T: UseCase.RequestValue, R: UseCase.ResponseValue>
+            execute(useCase: UseCase<T, R>, requestValue: T, callback: IUseCaseCallback<R>) {
+        useCase.requestValue = requestValue
+        //TODO add callback
+        useCaseScheduler.execute(Runnable {
+            useCase.run()
+        })
+    }
+
+    fun <T: UseCase.ResponseValue> notifyResponse(response: T, useCaseCallback: IUseCaseCallback<T>) {
+        useCaseScheduler.onResume(response, useCaseCallback)
+    }
+
+    fun <T: UseCase.ResponseValue> notifyError(useCaseCallback: IUseCaseCallback<T>) {
+        useCaseScheduler.onError(useCaseCallback)
+    }
 }
