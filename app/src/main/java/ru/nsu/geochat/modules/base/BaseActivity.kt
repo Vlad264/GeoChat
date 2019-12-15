ce14f16694764021193d6b2f5afc58074a48d256
Vlad <vlad-498@yandex.ru>
2019-12-15 23:45:04 +0700

    GC-19 Create BaseActivity

---
+++ app/src/main/java/ru/nsu/geochat/modules/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package ru.nsu.geochat.modules.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import ru.nsu.geochat.R
+
+abstract class BaseActivity(private val fragment: Fragment): AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, fragment)
+            .commit()
+
+    }
+
+    protected fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
