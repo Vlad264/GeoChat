3211d21e801daa26613ccb22a8a6f4a3f91f8fa7
Vlad <vlad-498@yandex.ru>
2019-12-19 18:12:38 +0700

    Create UseCase for login

---
+++ app/src/main/java/ru/nsu/geochat/modules/useCase/login/Login.kt
@@ -0,0 +1,31 @@
+package ru.nsu.geochat.modules.useCase.login
+
+import ru.nsu.geochat.common.useCaseEngine.UseCase
+import ru.nsu.geochat.modules.data.AuthInfo
+import ru.nsu.geochat.modules.data.Dao
+import ru.nsu.geochat.modules.data.login.ILoginDao
+
+class Login: UseCase<Login.RequestValue, Login.ResponseValue>() {
+
+    override fun executeUseCase(requestValue: RequestValue) {
+        Dao.instance.loginDao.tryLogin(
+            requestValue.name,
+            requestValue.password,
+            object : ILoginDao.ITryLoginCallback {
+                override fun onLogin(token: String?) {
+                    if (token != null) {
+                        AuthInfo.token = token
+                        useCaseCallback.onSuccess(ResponseValue())
+                    } else {
+                        useCaseCallback.onError()
+                    }
+                }
+
+            }
+        )
+    }
+
+    class RequestValue(val name: String, val password: String): UseCase.RequestValue
+
+    class ResponseValue: UseCase.ResponseValue
+}
