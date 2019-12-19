078703afd55884e93f0af963af4196077bca8601
Vlad <vlad-498@yandex.ru>
2019-12-20 02:29:07 +0700

    GC-35 Create list presenter

---
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/list/ListPresenter.kt
@@ -0,0 +1,7 @@
+package ru.nsu.geochat.modules.activity.main.list
+
+class ListPresenter(override val view: IListView) : IListPresenter {
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
