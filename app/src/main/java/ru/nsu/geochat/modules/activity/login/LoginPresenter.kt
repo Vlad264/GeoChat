bb9880e0bfd7e7634855fbf3f31d335a3540f77e
Vlad <vlad-498@yandex.ru>
2019-12-19 18:16:36 +0700

    Realized login presenter

--- app/src/main/java/ru/nsu/geochat/modules/activity/login/LoginPresenter.kt
+++ app/src/main/java/ru/nsu/geochat/modules/activity/login/LoginPresenter.kt
@@ -1,13 +1,32 @@
 package ru.nsu.geochat.modules.activity.login
 
-class LoginPresenter(override val view: ILoginView):
-    ILoginPresenter {
+import ru.nsu.geochat.common.useCaseEngine.IUseCaseCallback
+import ru.nsu.geochat.common.useCaseEngine.UseCaseHandler
+import ru.nsu.geochat.modules.useCase.login.Login
+
+class LoginPresenter(override val view: ILoginView): ILoginPresenter {
+
     override fun validatePassword(password: String): Boolean {
         return password.length > 4
     }
 
     override fun onLogin(login: String, password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val request = Login()
+        val requestValue = Login.RequestValue(login, password)
+        UseCaseHandler.instance.execute(
+            request,
+            requestValue,
+            object : IUseCaseCallback<Login.ResponseValue> {
+                override fun onSuccess(response: Login.ResponseValue) {
+                    view.onSuccessLogin()
+                }
+
+                override fun onError() {
+                    view.showErrorScreen("Error on login")
+                }
+
+            }
+        )
     }
 
     override fun start() {
