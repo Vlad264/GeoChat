4d7687de3204cd01d0a72cf2ed839a19e2595016
Vlad <vlad-498@yandex.ru>
2019-12-20 18:31:38 +0700

    GC-36 Fix list view

--- app/src/main/java/ru/nsu/geochat/modules/activity/main/list/IListView.kt
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/list/IListView.kt
@@ -1,9 +1,9 @@
 package ru.nsu.geochat.modules.activity.main.list
 
-import ru.nsu.geochat.models.Message
+import ru.nsu.geochat.models.Chat
 import ru.nsu.geochat.modules.base.IBaseView
 
 interface IListView: IBaseView<IListPresenter> {
-    fun onListLoaded(chats: List<Message>)
+    fun onListLoaded(chats: List<Chat>)
     fun onError()
 }
