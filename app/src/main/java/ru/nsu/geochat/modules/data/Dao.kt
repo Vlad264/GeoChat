3ebf0dc0b78253258405b6531e459a53e5f3bd5e
Vlad <vlad-498@yandex.ru>
2019-12-21 02:14:48 +0700

    GC-24 Add chat dao to shared dao

--- app/src/main/java/ru/nsu/geochat/modules/data/Dao.kt
+++ app/src/main/java/ru/nsu/geochat/modules/data/Dao.kt
@@ -1,13 +1,16 @@
 package ru.nsu.geochat.modules.data
 
+import ru.nsu.geochat.modules.data.chats.IChatsDao
+import ru.nsu.geochat.modules.data.chats.NetworkChatsDao
 import ru.nsu.geochat.modules.data.login.ILoginDao
 import ru.nsu.geochat.modules.data.login.NetworkLoginDao
 
 class Dao private constructor(
-    val loginDao: ILoginDao
+    val loginDao: ILoginDao,
+    val chatDao: IChatsDao
 ) {
     private object Holder {
-        val INSTANCE = Dao(NetworkLoginDao())
+        val INSTANCE = Dao(NetworkLoginDao(), NetworkChatsDao())
     }
 
     companion object {
