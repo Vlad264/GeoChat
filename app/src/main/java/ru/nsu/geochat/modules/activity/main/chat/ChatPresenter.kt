7e17085d1a53567af5eb64fc4d6307b5303b32c8
Vlad <vlad-498@yandex.ru>
2019-12-20 19:33:37 +0700

    GC-40 Create chat presenter

---
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/chat/ChatPresenter.kt
@@ -0,0 +1,14 @@
+package ru.nsu.geochat.modules.activity.main.chat
+
+import ru.nsu.geochat.models.Message
+
+class ChatPresenter(override val view: IChatView) : IChatPresenter {
+
+    override fun sendMessage(message: Message) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
