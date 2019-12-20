1b0fb35e095b01271b584e1803f3a1bd15be2fc8
Vlad <vlad-498@yandex.ru>
2019-12-21 02:34:53 +0700

    Realized list presenter

--- app/src/main/java/ru/nsu/geochat/modules/activity/main/list/ListPresenter.kt
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/list/ListPresenter.kt
@@ -1,7 +1,26 @@
 package ru.nsu.geochat.modules.activity.main.list
 
+import ru.nsu.geochat.common.useCaseEngine.IUseCaseCallback
+import ru.nsu.geochat.common.useCaseEngine.UseCaseHandler
+import ru.nsu.geochat.modules.useCase.chats.ChatsList
+
 class ListPresenter(override val view: IListView) : IListPresenter {
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val request = ChatsList()
+        val requestValue = ChatsList.RequestValue()
+        UseCaseHandler.instance.execute(
+            request,
+            requestValue,
+            object : IUseCaseCallback<ChatsList.ResponseValue> {
+                override fun onSuccess(response: ChatsList.ResponseValue) {
+                    view.onListLoaded(response.chats)
+                }
+
+                override fun onError() {
+                    view.onError()
+                }
+
+            }
+        )
     }
 }
