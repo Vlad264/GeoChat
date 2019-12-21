8c47a94b24ccdc134ab22bb694fe4c552675706b
Vlad <vlad-498@yandex.ru>
2019-12-21 09:59:48 +0700

    GC-59 Create fragment for create

--- app/src/main/java/ru/nsu/geochat/models/Chat.kt
+++ app/src/main/java/ru/nsu/geochat/models/Chat.kt
@@ -3,6 +3,8 @@ package ru.nsu.geochat.models
 data class Chat (
     val id: String,
     val title: String,
+    val description: String,
+    val destination: String,
     val lastMessage: String,
     val lastAuthor: String
 )
