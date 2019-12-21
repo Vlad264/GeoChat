acc637d4531810f85f2d1ee403ffd3c1e39b9e54
Vlad <vlad-498@yandex.ru>
2019-12-21 08:47:47 +0700

    GC-62 Create dao for message

--- app/src/main/java/ru/nsu/geochat/modules/data/Dao.kt
+++ app/src/main/java/ru/nsu/geochat/modules/data/Dao.kt
@@ -4,13 +4,16 @@ import ru.nsu.geochat.modules.data.chats.IChatsDao
 import ru.nsu.geochat.modules.data.chats.NetworkChatsDao
 import ru.nsu.geochat.modules.data.login.ILoginDao
 import ru.nsu.geochat.modules.data.login.NetworkLoginDao
+import ru.nsu.geochat.modules.data.messages.IMessagesDao
+import ru.nsu.geochat.modules.data.messages.NetworkMessagesDao
 
 class Dao private constructor(
     val loginDao: ILoginDao,
-    val chatDao: IChatsDao
+    val chatDao: IChatsDao,
+    val messageDao: IMessagesDao
 ) {
     private object Holder {
-        val INSTANCE = Dao(NetworkLoginDao(), NetworkChatsDao())
+        val INSTANCE = Dao(NetworkLoginDao(), NetworkChatsDao(), NetworkMessagesDao())
     }
 
     companion object {
---
+++ app/src/main/java/ru/nsu/geochat/modules/data/messages/NetworkMessagesDao.kt
@@ -0,0 +1,33 @@
+package ru.nsu.geochat.modules.data.messages
+
+import retrofit2.Response
+import ru.nsu.geochat.models.Message
+import ru.nsu.geochat.models.network.responses.MessageResponse
+import ru.nsu.geochat.modules.network.ApiService
+import java.io.IOException
+
+class NetworkMessagesDao: IMessagesDao {
+    override fun getMessagesList(id: String, callback: IMessagesDao.IGetMessagesCallback) {
+        val response: Response<List<MessageResponse>> = try {
+            ApiService.instance.getMessagesList(id).execute()
+        } catch (e: IOException) {
+            callback.onMessagesLoadad(emptyList())
+            return
+        }
+
+        if (response.isSuccessful) {
+            val body = response.body()
+            if (body != null) {
+                val result = mutableListOf<Message>()
+                for (m in body) {
+                    //TODO result.add(Message())
+                }
+                callback.onMessagesLoadad(result)
+            } else {
+                callback.onMessagesLoadad(emptyList())
+            }
+        } else {
+            callback.onMessagesLoadad(emptyList())
+        }
+    }
+}
