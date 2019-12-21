4ebb380072b81b4fe5f0acbd8d5adc78fcd9dd10
Vlad <vlad-498@yandex.ru>
2019-12-21 10:02:32 +0700

    GC-60 Create activity for create

---
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/create/CreateActivity.kt
@@ -0,0 +1,15 @@
+package ru.nsu.geochat.modules.activity.main.create
+
+import android.os.Bundle
+import ru.nsu.geochat.modules.base.BaseActivity
+
+class CreateActivity: BaseActivity(CreateFragment()) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val view = fragment as ICreateView
+        val presenter = CreatePresenter(view)
+        view.setPresenter(presenter)
+    }
+}
