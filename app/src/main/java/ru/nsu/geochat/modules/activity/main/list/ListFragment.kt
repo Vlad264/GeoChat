074fcfbefd6059d11e962825724004fd8a41d8ce
Vlad <vlad-498@yandex.ru>
2019-12-21 11:08:03 +0700

    GC-37 Create functionsl for create chat button

--- app/src/main/java/ru/nsu/geochat/modules/activity/main/list/ListFragment.kt
+++ app/src/main/java/ru/nsu/geochat/modules/activity/main/list/ListFragment.kt
@@ -1,5 +1,6 @@
 package ru.nsu.geochat.modules.activity.main.list
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_list.view.*
 import ru.nsu.geochat.R
 import ru.nsu.geochat.models.Chat
+import ru.nsu.geochat.modules.activity.create.CreateActivity
 import ru.nsu.geochat.modules.base.BaseActivity
 
 class ListFragment: Fragment(), IListView {
@@ -27,6 +29,11 @@ class ListFragment: Fragment(), IListView {
 
         list = view.recyclerList
         list.layoutManager = LinearLayoutManager(activity)
+
+        view.createChatButton.setOnClickListener {
+            val intent = Intent(context, CreateActivity::class.java)
+            activity?.startActivity(intent)
+        }
 
         return view
     }
